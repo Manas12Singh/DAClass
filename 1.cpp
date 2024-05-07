6d3425b2f6e29ed296d670ea120a7ab453808642
Manas12Singh <rd511030@gmail.com>
2024-05-07 11:21:20 +0530

    linear search done

--- 1.cpp
+++ 1.cpp
@@ -3,8 +3,7 @@
 
 using namespace std;
 
-int linearSearch(vector<int> &nums, int k)
-{
+int linearSearch(vector<int> &nums, int k) {
     int n = nums.size();
     for (int i = 0; i < n; i++)
         if (nums[i] == k)
@@ -12,9 +11,20 @@ int linearSearch(vector<int> &nums, int k)
     return -1;
 }
 
-int main()
-{
-    int n;
-    vector<int> nums;
+int main() {
+    int n, k;
+    cout << "Enter number of elements: ";
+    cin >> n;
+    vector<int> nums(n);
+    cout << "Enter the elements: ";
+    for (int i = 0; i < n; i++)
+        cin >> nums[i];
+    cout << "Enter the element to find: ";
+    cin >> k;
+    int index = linearSearch(nums, k);
+    if (index == -1)
+        cout << "Element not found." << endl;
+    else
+        cout << "Element found at index " << index << " ." << endl;
     return 0;
 }
