20fc805ca163783e16264b0a02728c7a1514cb1a
Dhruv <62509177+darkn3to@users.noreply.github.com>
2024-05-12 12:07:16 +0530

    Create 20.cpp

---
+++ 20.cpp
@@ -0,0 +1,12 @@
+const int INF = 1e9;
+
+void floydWarshall(vector<vector<int>>& dist, int n) {
+    for (int k = 0; k < n; k++) {
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < n; j++) {
+                if (dist[i][k] < INF && dist[k][j] < INF)
+                    dist[i][j] = min(dist[i][j], dist[i][k] + dist[k][j]);
+            }
+        }
+    }
+}
